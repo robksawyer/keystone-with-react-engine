0b4b0318620a500116c3b441796b79edc15ac960
Rob Sawyer <robksawyer@gmail.com>
2017-04-20 15:39:05 -0700

    Spacing cleanup

--- routes/hotloading.js
+++ routes/hotloading.js
@@ -1,17 +1,17 @@
 // start client webpack
- const config = require('../webpack.config');
- const webpack = require('webpack');
- const compiler = webpack(config);
+const config = require('../webpack.config');
+const webpack = require('webpack');
+const compiler = webpack(config);
 
 module.exports = (app) => {
     // setup the dev environment
     app.use(require('webpack-dev-middleware')(compiler, {
-        noInfo: false,
-        publicPath: config.output.publicPath,
-        historyApiFallback: true,
-        stats: {
-            colors: true,
-        },
+      noInfo: false,
+      publicPath: config.output.publicPath,
+      historyApiFallback: true,
+      stats: {
+        colors: true,
+      },
     }));
 
     // because hotloading is cool
