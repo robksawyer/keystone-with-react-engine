53cde88b8068f1231ca3abbba493be0bbe6b45b2
Rob Sawyer <robksawyer@gmail.com>
2017-04-20 15:37:22 -0700

    Updated to ES6

--- index.js
+++ index.js
@@ -1,40 +1,24 @@
-// Extended from:
-
-/*-------------------------------------------------------------------------------------------------------------------*\
-|  Copyright (C) 2016 PayPal                                                                                          |
-|                                                                                                                     |
-|  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     |
-|  with the License.                                                                                                  |
-|                                                                                                                     |
-|  You may obtain a copy of the License at                                                                            |
-|                                                                                                                     |
-|       http://www.apache.org/licenses/LICENSE-2.0                                                                    |
-|                                                                                                                     |
-|  Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   |
-|  on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  |
-|  the specific language governing permissions and limitations under the License.                                     |
-\*-------------------------------------------------------------------------------------------------------------------*/
-
-'use strict';
-
 // import the react-router routes
 // var Routes = require('./routes.jsx');
 
 // import the react-engine's client side booter
-var ReactEngineClient = require('react-engine/lib/client');
+// See https://github.com/paypal/react-engine#usage-on-client-side-mounting
+import ReactEngineClient from 'react-engine/lib/client';
 
 // boot options
-var options = {
+const options = {
   // routes: Routes,
 
   // supply a function that can be called
   // to resolve the file that was rendered.
+  // This should launch App.jsx
   viewResolver: function(viewName) {
     return require('./templates/views/' + viewName);
   }
 };
 
 document.addEventListener('DOMContentLoaded', function onLoad() {
+  console.log(options);
   // boot the app when the DOM is ready
   ReactEngineClient.boot(options);
 });
